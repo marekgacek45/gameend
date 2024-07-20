a1d90b3a2dd49794518a93d34eacd9e3a313b7d2
gacmen45 <marekgacek45@gmail.com>
2024-07-20 17:01:44 +0200

    create db schema and templates of resources

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,33 +2,43 @@
 
 namespace App\Providers\Filament;
 
-use Filament\Http\Middleware\Authenticate;
-use Filament\Http\Middleware\DisableBladeIconComponents;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
 use Filament\Panel;
+use Filament\Widgets;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Support\Facades\Blade;
+use Filament\Navigation\NavigationItem;
+use Filament\Navigation\NavigationGroup;
+use Filament\Http\Middleware\Authenticate;
+use Filament\Support\Facades\FilamentView;
+use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
-use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 
 class AdminPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
         return $panel
-            ->default()
+            ->spa()
             ->id('admin')
             ->path('admin')
             ->login()
+            ->passwordReset()
+            ->unsavedChangesAlerts()
+            ->sidebarCollapsibleOnDesktop()
+            ->brandLogo('/assets/logo.webp')
+            ->brandLogoHeight(fn () => auth()->check() ? '40px' : '100px')
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::hex('#fb2ec5'),
+                'gray' => Color::Slate
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
@@ -39,6 +49,23 @@ class AdminPanelProvider extends PanelProvider
             ->widgets([
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
+            ])
+            ->navigationGroups([
+                NavigationGroup::make()
+                     ->label('Treści'),
+                NavigationGroup::make()
+                    ->label('Gry'),
+                NavigationGroup::make()
+                    ->label('Kategorie i Tagi'),
+            ])
+            ->navigationItems([
+                NavigationItem::make('Strona Główna')
+                    ->url('http://localhost:8000', shouldOpenInNewTab: true)
+                    ->icon('heroicon-o-globe-alt'),
+                NavigationItem::make('Analityka')
+                    ->url('https://analytics.google.com/', shouldOpenInNewTab: true)
+                    ->icon('heroicon-o-chart-pie'),
+
             ])
             ->middleware([
                 EncryptCookies::class,
@@ -54,5 +81,11 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ]);
+    }
+
+    public function register(): void
+    {
+        parent::register();
+        FilamentView::registerRenderHook('panels::body.end', fn (): string => Blade::render("@vite('resources/js/app.js')"));
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tag;
+use App\Models\Post;
 use App\Models\User;
+use App\Models\Movie;
+use App\Models\TopGame;
+use App\Models\Category;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\CompletedGame;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,9 +21,17 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        Category::factory(10)->create();
+        CompletedGame::factory(10)->create();
+        Movie::factory(10)->create();
+        Post::factory(10)->create();
+        Tag::factory(10)->create();
+        TopGame::factory(10)->create();
+
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => 'admin123',
         ]);
     }
 }
